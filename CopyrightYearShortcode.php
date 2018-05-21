c8a2bdfbf3880de3c658f885def6bcdb876a40ab
alextryonpdx <alextryonpdx@gmail.com>
2018-05-21 15:37:56 -0700

    added ©

--- CopyrightYearShortcode.php
+++ CopyrightYearShortcode.php
@@ -3,7 +3,7 @@
  Plugin Name: WC Content Freeze
  Plugin URI: https://github.com/Watson-Creative/CopyrightYearShortcode
  GitHub Plugin URI: https://github.com/Watson-Creative/CopyrightYearShortcode
- description: A very simple plugin to dynamically inject '&copy;' and the current year in the footer copyright bar (or wherever needed)
+ description: A very simple plugin to dynamically inject '© ' and the current year in the footer copyright bar (or wherever needed)
  Version: 1.0.0
  Author: Alex Tryon
  Author URI: http://www.alextryonpdx.com
@@ -11,7 +11,7 @@
  */
 
 function copyright_year(){
-	echo '&copy; ' . date("Y");
+	echo '© ' . date("Y");
 }
 add_shortcode( 'copyright_year', 'copyright_year');
 
